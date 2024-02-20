81759c8bf788876fef3bc41b5ac653f306a8b67e
Harshit Anand <harshitanand283@gmail.com>
2024-02-20 22:41:26 +0530

    feat: user provider added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import Sidebar from "@/components/Sidebar";
 import SupabaseProvider from "@/providers/SupabaseProvider";
+import UserProvider from "@/providers/UserProvider";
+import ModalProvider from "@/providers/ModalProvider";
 import type { Metadata } from "next";
 import { Figtree } from "next/font/google";
 import "./globals.css";
@@ -16,7 +18,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 		<html lang="en">
 			<body className={font.className}>
 				<SupabaseProvider>
-					<Sidebar>{children}</Sidebar>
+					<UserProvider>
+						<ModalProvider />
+						<Sidebar>{children}</Sidebar>
+					</UserProvider>
 				</SupabaseProvider>
 			</body>
 		</html>
